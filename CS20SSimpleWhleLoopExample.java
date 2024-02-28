90de2818f9f4e1296285f3e1c75fd813dcfc9788
rsveinson <rsveinson@mts.net>
2024-02-28 09:44:03 -0600

    added random number and some commenting

--- CS20SSimpleWhleLoopExample.java
+++ CS20SSimpleWhleLoopExample.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import javax.swing.*;
 import java.io.*;
 import java.text.NumberFormat;
+import java.util.Random;
 
 /** ***************************************************
  *  Name:           Sveinson
@@ -18,74 +19,85 @@ import java.text.NumberFormat;
 public class CS20SSimpleWhleLoopExample {
 
     public static void main(String[] args) throws IOException{
-    // ***** constants *******
-    
-    // ***** variables *****
-    
+        // ***** constants *******
+
+        // ***** variables *****
+
         String banner = "";             // output banner
         String prompt = "";             // prompt for user input
-        
+
         String strin = "";              // string fro keyboard input
         String strout = "";             // string for formatted output
-        
+
         String delim = "[ ]+";          // delimiter for splitting input records
         String[] tokens = null;         // used to split input records
-        
+
         // a new line character that works on every computer system
         String nl = System.lineSeparator();
-        
+
         // ** example variables **
-        
+
         int n = 0;      // number read from keyboard
-    
-    // ***** objects *****
-    
+
+        // ***** objects *****
+
+        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         //NumberFormat currency = NumberFormat.getCurrencyInstance();
-    
-    // ***** print banners *****
-    
+
+        // ***** print banners *****
+
         banner = "*****************************" + nl;
         banner += "Name:        Sveinson" + nl;
         banner += "Class:       CS30S" + nl;
         banner += "Assignment:  Simple While Loop" + nl;
         banner += "*****************************" + nl + nl;
-        
+
         System.out.println(banner);
-    
-    // ***** Get Input *****
-    
-        prompt = "Enter a number, -1 to quit";
+
+        // ***** Get Input *****
+
+        //prompt = "Enter a number, -1 to quit";
+        prompt = "Generate some random nubmers between x and y";
         System.out.println(prompt);
-    
-    // ***** Main Processing *****
-    
+
+        // ***** Main Processing *****
+
+        // add pre-loop processing
+        // set counters to 0
+        // set summing variables to 0
+        // set other variables and values as needed
+
         // prime the loop a.k.a initialize the control variable
-        n = scanner.nextInt();
+        //n = scanner.nextInt();
+        n = random.nextInt(50);     // generate a random number between x and y
         //System.out.println(n);
-        
-        while(n >= -1){
-            System.out.println("here are the instructions ");
-            System.out.println("to be repeated. Calculations,");
-            System.out.println("formatted output, etc..");
-            
-            // formatted output might go here
+
+        while(n != 1){         // n == 0
+
             System.out.println("The number is " + n);
             
+            // counting iterations
+            // event counting
+            
+            // iteration sum
+            // event sum
+
             // update the control variable
             System.out.println(prompt);
-            n = scanner.nextInt();           
+            //n = scanner.nextInt();
+            n = random.nextInt(50) + 1;     // generate a random number between x and y           
         }// end while not 0
-    
+
         // post-loop processing
         // i.e. calcualte the average of the numbers entered
-    
-    // ***** Print Formatted Output *****
-    
+
+        // ***** Print Formatted Output *****
+
         // print summary info
-    
-    // ***** Closing Message *****
-    
+
+        // ***** Closing Message *****
+
         System.out.println();
         System.out.println("end of processing");
 
